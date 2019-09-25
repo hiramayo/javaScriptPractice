883ca660b40d34c191e2711f00a4ce4afe3d45c0
hiramayo <hiramayo@gmail.com>
2019-09-25 22:21:58 +0900

    add chapter3-2

---
+++ _includes/js/chapter3/chapter3-2.js
@@ -0,0 +1,16 @@
+//コンストラクタ
+var Person = function (name, year){
+    this.name = name;
+    this.year = year;
+}
+//コンストラクタのプロトタイプにメソッドを追加
+Person.prototype.greeting = function() {
+    return "I am " + this.name + ". I am " + this.year + "years old."
+}
+
+//オブジェクトの作成＆greet
+function display(){
+    var hiramayo = new Person("hiramayo",33),
+        result = document.getElementById("result");
+    result.innerText = hiramayo.greeting();
+}
