55128ef88d3aef4ef0b5dc7769dd1d0ed4d9f47f
hiramayo <hiramayo@gmail.com>
2020-06-16 22:51:58 +0900

    add chapter4-2-1

--- _includes/js/chapter4/chapter4-2-1.js
+++ _includes/js/chapter4/chapter4-2-1.js
@@ -2,7 +2,7 @@
 function findNode(callBack) {
     var nodeList = document.querySelectorAll("input");
     if(typeof callBack === "function"){
-        nodeList.forEach(hide);
+        nodeList.forEach(callBack);
     }
     return nodeList;
 }
@@ -15,7 +15,34 @@ function hide(currentValue, currentIndex, listObj){
     console.log(listObj);
 }
 
+//nodeの配列を取得
+function findNodeArray(callBack) {
+    var nodeList = document.querySelectorAll("p"),
+        nodeArray = [];
+
+    nodeList.forEach(function(node,index){
+        if(typeof callBack === "function"){
+            nodeList.forEach(callBack);
+        }
+        nodeArray.push(node);
+    })
+
+    return nodeArray;
+}
+
+//nodeListのforEachで呼び出されるcallBack
+//nodeList.prototype.forEachには、以下の３つのパラメータが設定可能。
+function color(node){
+    node.style.color = "green";
+}
+
+
 //nodeを隠す
 function execute(){
     findNode(hide);
+}
+
+//nodeを緑色に
+function execute2(){
+    findNodeArray(color);
 }
